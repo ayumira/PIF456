3a6374f45e9788673b15dc3653df5e9c22a5e3df
Mira Ayu <Maihimemira@gmail.com>
2013-10-16 23:38:06 +0700

    Modul 1

---
+++ SK1.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+
+<title>Greeting</title>
+</head>
+<body>
+
+<center>
+
+<h1>
+<?php
+function greeting()
+{
+$date = date ("G : i A");
+if ($date>=0 and $date<10) {
+echo "Selamat Pagi";
+} else if ($date>=10 and $date<15) {
+echo "Selamat Siang";
+} else if ($date>=15 and $date<19) {
+echo "Selamat Sore";
+} else if ($date>=19 and $date<00) {
+echo "Selamat Malam";
+}else echo "Waktu salah)";
+}
+?>
+<?php greeting(); ?><br>
+
+</h1>
+<h3> Sekarang adalah
+
+<?php
+
+//Array Hari
+$array_hari = array(1=>"Senin","Selasa","Rabu","Kamis","Jumat", "Sabtu","Minggu");
+$hari = $array_hari[date("N")];
+//Format Tanggal
+$tanggal = date ("j");
+//Array Bulan
+$array_bulan = array(1=>"Januari","Februari","Maret", "April", "Mei", "Juni","Juli","Agustus","September","Oktober", "November","Desember");
+$bulan = $array_bulan[date("n")];
+//Format Tahun
+$tahun = date("Y");
+//Menampilkan hari dan tanggal
+echo "hari $hari, tanggal $tanggal $bulan $tahun";
+?>
+<br>
+
+Waktu menunjukkan pukul
+<?php
+//penulisan waktu
+$date = date ("G : i A");
+echo "$date WIB";
+?>
+
+</h3>
+</body>
+
+</html>
